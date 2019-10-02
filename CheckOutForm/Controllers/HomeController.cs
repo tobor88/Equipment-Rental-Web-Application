7b9af002634a8873f60f858eec3614d65c55c105
tobor <rosborne@osbornepro.com>
2019-10-02 11:46:25 -0600

    Update HomeController.cs

--- CheckOutForm/Controllers/HomeController.cs
+++ CheckOutForm/Controllers/HomeController.cs
@@ -24,6 +24,27 @@ namespace CheckOutForm.Controllers
             return View();
         }
 
+        static bool mailSent = false;
+
+        private static void SendCompletedCallback(object sender, AsyncCompletedEventArgs e)
+        {
+            // Get the unique identifier for this asynchronous operation.
+            String token = (string)e.UserState;
+
+            if (e.Cancelled)
+            {
+                Console.WriteLine("[{0}] Send canceled.", token);
+            }
+            if (e.Error != null)
+            {
+                Console.WriteLine("[{0}] {1}", token, e.Error.ToString());
+            }
+            else
+            {
+                Console.WriteLine("Message sent.");
+            }
+            mailSent = true;
+        }
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Index(CheckoutFormModel e)
@@ -32,16 +53,22 @@ namespace CheckOutForm.Controllers
             {
                 try
                 {
-                    var Requestermessage = new MimeMessage();
-                    Requestermessage.From.Add(new MailboxAddress("Laptop Form", "laptopform@osbornepro.com"));
-                    Requestermessage.To.Add(new MailboxAddress(e.Name, e.Email));
-                    Requestermessage.Subject = "Check Out Request Made";
 
-                    var builder = new BodyBuilder();
-                    var image = builder.LinkedResources.Add(@"C:\Users\Public\source\repos\CheckOutForm\CheckOutForm\wwwroot\images\logobannercolored.png");
-                    image.ContentId = MimeUtils.GenerateMessageId();
+                    SmtpClient client0 = new SmtpClient("mail.smtp2go.com", 2525);
+                    SmtpClient client1 = new SmtpClient("mail.smtp2go.com", 2525);
+                    SmtpClient client2 = new SmtpClient("mail.smtp2go.com", 2525);
 
-                    builder.HtmlBody = string.Format($@"<p><strong>{e.Name},</strong></p>
+                    MailAddress ithr = new MailAddress("laptopform@osbornepro.com", "OsbornePro Form ", System.Text.Encoding.UTF8);
+                    MailAddress requester = new MailAddress(e.Email, e.Name, System.Text.Encoding.UTF8);
+                    MailAddress manager = new MailAddress(e.Manager, "Manager", System.Text.Encoding.UTF8);
+
+                    MailMessage message0 = new MailMessage(ithr, requester);
+                    MailMessage message1= new MailMessage(requester, ithr);
+                    MailMessage message2 = new MailMessage(ithr, manager);
+
+                    Attachment policyDoc = new Attachment("C:\\Users\\Public\\Documents\\LaptopPolicy.docx");
+
+                    message0.Body = string.Format($@"<p><strong>{e.Name},</strong></p>
                         <p>Your request has been received. You will be contacted after your request is processed.</p>
                         <p>Your Request details are listed below. The OsbornePro Laptop Policy is also attached to this email. <br>
                 <br>
@@ -65,22 +92,16 @@ namespace CheckOutForm.Controllers
                 <br>
                         Thank you for your submission,            <br>
                         - OsbornePro IT Department            <br>
-                        <img src=""cid:{0}""></center>", image.ContentId);  // End mailbody
+                        <img src=""cid:{0}""></center>");  // End mailbody
 
-                    builder.Attachments.Add(@"C:\Users\Public\Documents\LaptopPolicy.docx");
-                    Requestermessage.Body = builder.ToMessageBody();
+                    message0.Attachments.Add(policyDoc);
+                    message0.Body += Environment.NewLine ;
+                    message0.IsBodyHtml = true;
+                    message0.BodyEncoding = System.Text.Encoding.UTF8;
+                    message0.Subject = "Equipment Check Out Request Made";
+                    message0.SubjectEncoding = System.Text.Encoding.UTF8;
 
-
-                    var message = new MimeMessage();
-                    message.From.Add(new MailboxAddress(e.Name, e.Email));
-                    message.To.Add(new MailboxAddress("Laptop Form", "laptopform@osbornepro.com"));
-                    message.Subject = "Check Out Request";
-
-                    var builder1 = new BodyBuilder();
-                    var image1 = builder1.LinkedResources.Add(@"C:\Users\Public\source\repos\CheckOutForm\CheckOutForm\wwwroot\images\logobannercolored.png");
-                    image1.ContentId = MimeUtils.GenerateMessageId();
-
-                    builder1.HtmlBody = string.Format($@"<p><strong>HR and IT,</strong></p>
+                    message1.Body = string.Format($@"<p><strong>HR and IT,</strong></p>
                         <p>Equipment request was submitted for {e.Name}. The details are below. <br> To approve this request, forward this email to help@osbornepro.com or click the Apply link to autogenerate the approval email.</p>
                         <p>Your Request details are listed below.
                 <br>
@@ -106,7 +127,7 @@ namespace CheckOutForm.Controllers
 
                     <span>
                         <a href=""mailto:help@osbornepro.com?cc={e.Email}&subject=Approved%20Request&body={@e.Name}s%20Equipment%20Rental%20has%20been%20approved!%0A%0A{@e.Reasons}""><strong>Approve Request</strong>
-                        <br> </a>
+                        <br> </a> 
                     </span>
 
                     <span>
@@ -115,24 +136,18 @@ namespace CheckOutForm.Controllers
                     </span>
 
                     </div>
-                        <img src=""cid:{1}""></center>", image1.ContentId);  // End mailbody
+                        <img src=""cid:{1}""></center>");  // End mailbody
+                    message1.Body += Environment.NewLine;
+                    message1.IsBodyHtml = true;
+                    message1.BodyEncoding = System.Text.Encoding.UTF8;
+                    message1.Subject = "Equipment Check Out Request Made";
+                    message1.SubjectEncoding = System.Text.Encoding.UTF8;
 
-                    message.Body = builder1.ToMessageBody();
-
-                    var Managermessage = new MimeMessage();
-                    Managermessage.From.Add(new MailboxAddress(e.Name, e.Email));
-                    Managermessage.To.Add(new MailboxAddress("", e.Manager));
-                    Managermessage.Subject = "Check Out Request Made";
-
-                    var builder2 = new BodyBuilder();
-                    var image2 = builder2.LinkedResources.Add(@"C:\Users\Public\source\repos\CheckOutForm\CheckOutForm\wwwroot\images\logobannercolored.png");
-                    image2.ContentId = MimeUtils.GenerateMessageId();
-                    // Set the plain-text version of the message text
-                    builder2.HtmlBody = string.Format($@"<p><strong>{e.Manager},</strong></p>
+                    message2.Body = string.Format($@"<p><strong>{e.Manager},</strong></p>
                 <p>{e.Name} has submitted a device request form to HR. The details are listed below. <br>
                 If you wish to make any changes please alert HR and IT as well as {e.Name}. <br>
                 If you wish to cancel this request click the Deny Request link at the bottom of this email to autogenerate the email to send. <br>
-                If you click deny and Microsoft's Mail app opens change your default email application to Outlook. This can be done by following the instructions <a href=""https://helpdesk.osbornepro.com/articles/file-does-not-have-an-application-associated-with-it"">HERE</a> or <a href=""https://helpdesk.osbornepro.com/articles/change-default-applicaiton"">HERE</a>
+                If you click deny and Microsoft's Mail app opens change your default email application to Outlook. This can be done by following the instructions <a href=""https://helpdesk.usav.org/kb/articles/file-does-not-have-an-application-associated-with-it"">HERE</a> or <a href=""https://helpdesk.usav.org/kb/articles/change-default-applicaiton"">HERE</a>
 
                 < br>
                             <strong>Name:</strong> {e.Name} <br>
@@ -154,29 +169,58 @@ namespace CheckOutForm.Controllers
                             <strong>Message:</strong> ""{e.Information}""           <br>
                 <br>
                 If you have any concerns, updates, or changes to this request; or do not approve of this request; let HR and/or the IT Department know as soon as possible.</p>
-
+                    
                     <span>
                         <a href=""mailto:hr@osbornepro.com?cc={e.Email}&subject=Manager%20denied%20Request&body=We%20are%20sorry%20{@e.Name}%20your%20Equipment%20Request%20has%20been%20denied.""><strong>Deny Request</strong>
                         <br> </a>
                     </span>
 
-                        <img src=""cid:{2}""></center>", image2.ContentId);  // End mailbody
-                                                                             // Use to attach a file    builder2.Attachments.Add(@"C:\Users\Public\source\repos\CheckOutForm\CheckOutForm\wwwroot\images\logobanner.png");
-                    Managermessage.Body = builder2.ToMessageBody();
+                        <img src=""cid:{2}""></center>");  // End mailbody
+                    message2.Body += Environment.NewLine;
+                    message2.IsBodyHtml = true;
+                    message2.BodyEncoding = System.Text.Encoding.UTF8;
+                    message2.Subject = "Equipment Check Out Request Made";
+                    message2.SubjectEncoding = System.Text.Encoding.UTF8;
 
-                    //Start email Thread
-                    using var smtp = new MailKit.Net.Smtp.SmtpClient();
-                    smtp.Connect("smtp2go.com", 2525, SecureSocketOptions.StartTls);
-                    smtp.Timeout = 3000;
-                    smtp.AuthenticationMechanisms.Remove("XOAUTH2");
-                    smtp.Send(message);
-                    smtp.Send(Managermessage);
-                    smtp.Send(Requestermessage);
-                    smtp.Disconnect(true);
+                    client0.SendCompleted += new
+                    SendCompletedEventHandler(SendCompletedCallback);
+                    client1.SendCompleted += new
+                    SendCompletedEventHandler(SendCompletedCallback);
+                    client2.SendCompleted += new
+                    SendCompletedEventHandler(SendCompletedCallback);
+
+                    string userState0 = "Emails sending to inform user their request was submitted... ";
+                    string userState1 = "Emails sending to inform HR for approval of laptop request... ";
+                    string userState2 = "Emails sending to the requesters manager informting them of the request... ";
+
+                    client0.SendAsync(message0, userState0);
+                    client1.SendAsync(message1, userState1);
+                    client2.SendAsync(message2, userState2);
+
+                    Console.WriteLine("Sending message... press c to cancel mail. Press any other key to exit.");
+
+                    string answer = Console.ReadLine();
+
+                    if (answer.StartsWith("c") && mailSent == false)
+                    {
+                        client0.SendAsyncCancel();
+                        client1.SendAsyncCancel();
+                        client2.SendAsyncCancel();
+                    }
+
+                    message0.Dispose();
+                    message1.Dispose();
+                    message2.Dispose();
+                    
+                    client0.Dispose();
+                    client1.Dispose();
+                    client2.Dispose();
+
+                    Console.WriteLine("Goodbye.");
                 } // End try
                 catch (Exception)
                 {
-                    return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+                    return View("Error");
                 }
             }
 
